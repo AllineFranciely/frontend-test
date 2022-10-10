2ae25bbf780d32767d9ccddfa6c3cd1afafa4a1e
Alline <alline_franciely@hotmail.com>
2022-10-10 19:13:48 -0300

    finaliza responsividade

--- clubbi-challenge/src/components/Card/Card.js
+++ clubbi-challenge/src/components/Card/Card.js
@@ -4,7 +4,7 @@ import './Card.css';
 function Card({ title, image, originalTitle, originalTitleRomanised, director, year, nota, description, duracao }) {
   return (
     <div className="Card" data-testid="Card">
-      <h1>{title}</h1>
+      <h1 className="title">{title}</h1>
       <img className="filmIMG" src={image} alt={`imagem do filme${title}`} />
       <p data-testid="tituloOriginal">{originalTitle} - {originalTitleRomanised}</p>
       <p data-testid="diretor">Diretor: {director}</p>
